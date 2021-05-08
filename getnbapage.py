e7d3bad94506785973e404c1a62f138191bf23da
Derrick Shu <52884708+liangshu23@users.noreply.github.com>
2021-05-08 23:11:11 +0900

    Update getnbapage.py

--- getnbapage.py
+++ getnbapage.py
@@ -1,3 +1,5 @@
+## FOR WALK THROUGH PLEASE REFER TO getnbapage.ipynb ##
+
 import time
 import bs4 as bs
 import pandas as pd
@@ -10,12 +12,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 Path = 'D:/____________/chromedriver.exe'
 driver = webdriver.Chrome(executable_path = Path)
 # link for the page you want to scrape data from
-
-# season total shooting by zone
+    # season total shooting by zone
 link = 'https://www.nba.com/stats/players/shooting/?Season=2020-21&SeasonType=Regular%20Season&PerMode=Totals'
 
-# link = 'https://www.nba.com/stats/players/shooting/?Season=2020-21&SeasonType=Regular%20Season'
-
+# opening page & getting rid of cookiepolicy pop-up
 driver.get(link)
 try:
     WebDriverWait(driver,15).until(
@@ -33,7 +33,7 @@ time.sleep(1.5)
 criteria = '<tr aria-hidden="false"'
 
 temp = []
-
+# identify all table rows and isloate text & numbers
 def scrapepage():
     source = bs.BeautifulSoup(driver.page_source, 'html.parser').find('table')
     whole = [b for b in source.find_all('tr')]
@@ -56,7 +56,7 @@ def scrapepage():
             #print(team_data)
             temp.append(row_text)
 
-
+# find and click to move onto the next page
 def nextpage():
     driver.implicitly_wait(3)
     next = driver.find_element_by_class_name('stats-table-pagination__next')
@@ -72,27 +72,30 @@ for i in range (0,11):
 
 print(temp)
 
-# Advanced_stats_columns = ['blank','PLAYER', 'TEAM', 'AGE', 'GP', 'W', 'L', 'MIN', 'OFFRTG', 'DEFRTG', 	
-# 'NETRTG', 'AST%', 'AST/TO', 'AST Ratio', 'OREB%', 'DREB%', 'REB%', 'TO Ratio', 'eFG%', 'TS%', 'USG%', 'PACE', 'PIE','bl']
+    # Column names you can use!
 
-# hustle_columns = ['blank', 'Player', 'TEAM', 'AGE', 'GP', 'MIN', 'Screen Assists', 'Screen Assists PTS', 'Deflections', 'OFF Loose BallsRecovered',
-# 'DEF Loose Balls Recovered', 'Loose Balls Recovered', 'Percent Loose BallsRecovered OFF', 'Percent Loose BallsRecovered DEF',
-# 'Charges Drawn', 'Contested 2PT Shots', 'Contested 3PT Shots', 	'Contested Shots', 'blank2']
+# Advanced_stats_columns = ['Blank','PLAYER', 'TEAM', 'AGE', 'GP', 'W', 'L', 'MIN', 'OFFRTG', 'DEFRTG', 	
+# 'NETRTG', 'AST%', 'AST/TO', 'AST Ratio', 'OREB%', 'DREB%', 'REB%', 'TO Ratio', 'eFG%', 'TS%', 'USG%', 'PACE', 'PIE','Blank2']
+
+# hustle_columns = ['Blank', 'Player', 'TEAM', 'AGE', 'GP', 'MIN', 'Screen.Assists', 'Screen.Assists.PTS', 'Deflections', 'OFF.Loose.Balls.Recovered',
+# 'DEF.Loose.Balls.Recovered', 'Loose.Balls.Recovered', 'Percent Loose.Balls.Recovered.OFF', 'Percent.Loose.Balls.Recovered.DEF',
+# 'Charges.Drawn', 'Contested.2PT.Shots', 'Contested.3PT.Shots', 'Contested.Shots', 'Blank2']
 
 # Basic_Stats_columns = ['Blank','Player','Team','GP','W','L','Minutes','Drives','FGM','FGA','FG%','FTM','FTA','FT%',
 #             'PTS','PTS%','PASS','PASS%','Assist','AST%','TO','TOV%','PF','PF%','Blank2']
 
-# player_bio_columns = ['blank','PLAYER', 'TEAM', 'AGE', 'Height', 'Weight', 'College', 'Country', 'DraftYear', 'DraftRound', 	
-# 'DraftSpot', 'GP', 'PTS', 'REB', 'AST', 'NETRTG', 'OREBper', 'DREBper', 'USG', 'TS', 'ASTper','bl']
+# player_bio_columns = ['Blank','PLAYER', 'TEAM', 'AGE', 'Height', 'Weight', 'College', 'Country', 'DraftYear', 'DraftRound', 	
+# 'DraftSpot', 'GP', 'PTS', 'REB', 'AST', 'NETRTG', 'OREBper', 'DREBper', 'USG', 'TS', 'ASTper','Blank2']
 
 shooting_by_5ft_columns = ['Blank','Player','Team','Age','LT5 FGM','LT5 FGA','LT5 prct', 'Five-Nine FGM','Five-Nine FGA','Five-Nine prct',
            'Ten-Fourteen FGM','Ten-Fourteen FGA','Ten-Fourteen prct','Fifteen-Nineteen FGM','Fifteen-Nineteen FGA','Fifteen-Nineteen prct',
            'Twenty-TwentyFour FGM','Twenty-TwentyFour FGA','Twenty-TwentyFour prct','TwentyFive-TwentyNine FGM',
            'TwentyFive-TwentyNine FGA','TwentyFive-TwentyNine prct','Blank2']
-
+# convert temp into dataframe
 df = pd.DataFrame(temp, columns = shooting_by_5ft_columns)
 df = df.drop(columns=['Blank','Blank2'])
 
+#get row numbers with header rows we want to remove
 def get_rep_title_axis(dfname):
     wrong_rows = []
     for i in range(0, dfname.shape[0]):
@@ -105,7 +108,7 @@ def get_rep_title_axis(dfname):
 
     return wrong_rows
     
-   
+# removing header rows and index column
 df = df.drop(index = get_rep_title_axis(df)).reset_index()
 df = df.drop(columns = ['index'])
 
